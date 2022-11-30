b24a61dff8db9bf173da3c63f639278d3ff2c4a2
kes91626 <kes91626@gmail.com>
2022-11-30 17:42:23 +0900

    # Price Chart Two part Two(1/2)

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,17 +1,55 @@
 import { useQuery } from "@tanstack/react-query";
 import { useOutletContext, useParams } from "react-router-dom";
 import { fetchCoinHistory } from "./api";
+import ApexChart from "react-apexcharts";
+import Price from "./Price";
+
+interface IdData {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 interface coinInterface {
   coinId: string;
 }
 function Chart() {
   const coinId = useOutletContext<coinInterface>().coinId;
-  const { isLoading, data } = useQuery(["", coinId], () =>
+  const { isLoading, data } = useQuery<IdData[]>(["", coinId], () =>
     fetchCoinHistory(coinId)
   );
   console.log(data);
-  return <h1>hi</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "sales",
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 500,
+              width: 500,
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
